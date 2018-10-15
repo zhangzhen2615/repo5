3b4618eccba9998e48ee31def885b6a68776ce88
zhangzhen <1137643278@qq.com>
2018-10-15 23:48:25 +0800

    MavenGit3.0版本

--- MavenGit/MavenGitDao/src/main/java/Demo.java
+++ MavenGit/MavenGitDao/src/main/java/Demo.java
@@ -3,6 +3,9 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("git添加成功");
         System.out.println("Maven1.0版本成功提交");
+        System.out.println("推送奥喏破");
+
+
     }
 
 }
